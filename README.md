d5c95064f329607dedacca28bcdb9e746220ec58
James Vaughn <jvaughn575@gmail.com>
2017-12-12 11:52:26 -0600

    Initial express api and react routes

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# travel_hive
+
+# Travel Hive
+===================
+
+- - - -
+
+# Steps to run: #
+
+The /src directory contains the react app and also a folder /express-api
+that holds the express api endpoint.
+
+// Nodemon will watch files in the express-script directory and restart
+// the server when files change
+1. npm install nodemon -g  
+
+// Starts express
+2. In the express-api folder run: npm start
+
+// Starts react app
+3. In the /src folder run: npm start
+
+Then localhost:3000/ will take you to the home page.
+
+localhost:3000/api/version will fetch the version number from the express api.
--- travel_hive/src/App.js
+++ travel_hive/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">TRAVEL HIVE</h1>
-        </header>
-        <p className="App-intro">
-          Get your travel on!
-        </p>
-      </div>
+      <Router>
+            <div>
+                <Route path="/" component={Home}/>
+                <Route path="/api/version" component={ApiVersion} />
+            </div>
+      </Router>
     );
   }
 }
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">TRAVEL HIVE</h1>
+              </header>
+              <p className="App-intro">
+                Get your travel on!
+              </p>
+            </div>
+        )
+    }
+}
+
+class ApiVersion extends Component {
+    state = { apiVersion: [] }
+
+    componentDidMount() {
+        fetch('/api/version')
+            .then(res => res.json())
+            .then(apiVersion => this.setState({ apiVersion }));
+    }
+
+    render(){
+        return(
+            <div>
+                <p>{ this.state.apiVersion.version }</p>
+            </div>
+        );
+    }
+}
+
 
 export default App;
