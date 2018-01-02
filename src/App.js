518460fb1ce9ee3083cd059e653504728c730a44
Maroua Jawadi <marouajawadi@gmail.com>
2018-01-02 15:49:57 -0600

    added Plan, Experience, Connect pages and routing

--- src/App.js
+++ src/App.js
@@ -12,9 +12,12 @@ import {
   Link,
 } from 'react-router-dom'
 
-import {WrappedRegistrationForm} from './components/Join';
-import {Inspiration} from './components/Inspiration';
-import {WrappedLoginForm} from './components/Login';
+import {WrappedRegistrationForm} from './components/Pages/Join';
+import {WrappedLoginForm} from './components/Pages/Login';
+import {Inspiration} from './components/Pages/Inspiration';
+import {Plan} from './components/Pages/Plan';
+import {Experience} from './components/Pages/Experience';
+import {Connect} from './components/Pages/Connect';
 
 const DefaultLayout = ({children}) => (
   <div>
@@ -35,6 +38,9 @@ const App = () => (
         <Route path="/join" component={WrappedRegistrationForm} />
         <Route path="/login" component={WrappedLoginForm} />
         <Route path="/inspiration" component={Inspiration} />
+        <Route path="/plan" component={Plan} />
+        <Route path="/experience" component={Experience} />
+        <Route path="/connect" component={Connect} />
         <Route path="/api/version" component={ApiVersion} />
   </div>
 </DefaultLayout>
@@ -59,13 +65,19 @@ export const AppHeader = () => (
 
   <div className="menu">
     <Menu
-      theme="light"
       mode="horizontal"
       defaultSelectedKeys={['1']}
-      style={{ lineHeight: '64px'}}
+      style={{ lineHeight: '64px',
+               border: '1px solid #DCDCDD',
+               padding: '0 25px',
+               display: 'flex',
+               justifyContent: 'flex-start',
+            }}
     >
       <Menu.Item key="1"><Link to="/inspiration">Inspiration</Link></Menu.Item>
-      <Menu.Item key="2">Plan</Menu.Item>
+      <Menu.Item key="2"><Link to="/plan">Plan</Link></Menu.Item>
+      <Menu.Item key="3"><Link to="/experience">Experience</Link></Menu.Item>
+      <Menu.Item key="4"><Link to="/connect">Connect</Link></Menu.Item>
     </Menu>
   </div>
 
