00fa8c06d97440907749a940398d38ad2808c8b3
James Vaughn <jvaughn575@gmail.com>
2018-01-16 20:26:16 -0600

    Refactored server and Passport Strategy config code

--- server/server.js
+++ server/server.js
@@ -5,48 +5,79 @@ import passport from '../config/passportStrategy';
 const app = express();
 const port = process.env.PORT || 3001;
 
-const flash = require('connect-flash')
-
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
 // Passport requirements
 // Not secure must change before production
-app.use(session({ secret: 'jk_travelhive'}))
+app.use(session({ 
+    secret: 'jk_travelhive',
+    resave: false,
+    saveUninitialized: false,
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+ });
 
 // Router
 const router = express.Router();
 
-// Routes
-router.get('/version', function(req, res){
+/*********Debug Logging middleware***********/
+app.use((req, res, next) => {
+    const status = req.isAuthenticated() ? 'logged in' : 'logged out';
+    console.log(
+      'status:', status, '\n',
+      req.sessionStore,
+      req.sessionID,
+      req.session
+    );
+    next();
+  });
+
+let showRequest = (req,res,next) => {
+    console.log(req.session);
+    next();
+}
+/****************************************** */
+
+
+/*************** Routes*******************/
+router.get('/version', (req, res) => {
     res.json({ version: "1.0.0"});
 });
 
-router.post('/join', (req, res, next) => 
-    passport.authenticate('local-signup', (err, user, response) => {
-        res.status(response.code).json({  
-            code: response.code,          
-            message: response.message    
-        });
-     })(req, res, next)
-);    
+router.get('/loggedIn', (req,res) => {
+    res.json({ loggedIn: req.isAuthenticated() });
+});
 
-router.post('/login', (req, res, next) => 
-    passport.authenticate('local-login', (err, user, response) => {
-        res.status(response.code).json({  
-            code: response.code,          
-            message: response.message    
-        });
-    })(req, res, next)
-);       
+router.post('/join', passport.authenticate('local-join'), 
+        (req,res) => {
+            res.json({                  
+                message: "join"                
+            });
+        }
+);      
+    
+router.post('/login', passport.authenticate('local-login'), 
+        
+        (req, res) => {            
+            res.json({                   
+                message: "login",
+                user: req.user,
+                isLoggedIn: req.isAuthenticated()
+            });
+        }   
+);    
+/*****************************************/   
     
 // Register all routes with api prefix
 app.use('/api', router);
