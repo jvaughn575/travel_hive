c33f371cef932b909c080db7ba98eefdffc29848
Reggie <reggie.a.roby@gmail.com>
2018-01-21 04:46:07 -0600

    refactor server init code

--- config/passportStrategy.js
+++ config/passportStrategy.js
@@ -2,95 +2,92 @@
 import LocalStrategy from 'passport-local';
 import passport from 'passport';
 
-// Load user model
-import User from '../server/models/userModel';
+export let passportStrat = async function(User){
+    /*****setup for persistent login sessions for passport******/
+    passport.serializeUser((user, done) => {
+        console.log("!!!!!!!!   SERIALIZING  !!!!!!!!!!")
+        return done(null, user.id);
+    });
 
-/*****setup for persistent login sessions for passport******/
-passport.serializeUser((user, done) => {
-    console.log("!!!!!!!!   SERIALIZING  !!!!!!!!!!")
-    return done(null, user.id);
-});
+    passport.deserializeUser(async (id, done) => {    
+        console.log("!!!!!!!!   DESERIALIZING   !!!!!!!!!!");
+        const user = await User.findOne({
+            where: { id : id }
+            })
+        return done(null, user);
+    });
+    /**************************************************/
 
-passport.deserializeUser(async (id, done) => {    
-    console.log("!!!!!!!!   DESERIALIZING   !!!!!!!!!!");
-    const user = await User.findOne({
-        where: { id : id }
+    /************** Helper Functions ******************/
+    const createUser = async (username, email, password) => {
+        const user = await User.create({
+                        username: username,
+                        email: email,
+                        password: User.generateHash(password)
+                     }).then( user => {return user});
+        return user;
+            
+    };
+
+    const getUserWithEmail =  async (email) => {
+        const user = await User.findOne({
+            where: {email : email }
         })
-    return done(null, user);
-});
-/**************************************************/
+        return user;
+    }   
+    /**************************************************/
 
-/************** Helper Functions ******************/
-const createUser = async (username, email, password) => {
-    const user = await User.create({
-                    username: username,
-                    email: email,
-                    password: User.generateHash(password)
-                 }).then( user => {return user});
-    return user;
-        
-};
-
-const getUserWithEmail =  async (email) => {
-    const user = await User.findOne({
-        where: {email : email }
-    })
-    return user;
-}   
-/**************************************************/
-
-/***********Passport Strategy Local-Join************/
-passport.use('local-join', new LocalStrategy({
-    usernameField : 'email',
-    passwordField : 'password',
-    passReqToCallback : true
-},
-(req, email, password, done) => {     
-    // get user with given email. If user already exists return false  
-    getUserWithEmail(email)
-        .then(user => {
-            console.log("Passport local-join",user);  
-            if (user) {
-                console.log("User already Exists");
-                return done(null, false);    
-            }    
-
-            // else create and return user                    
-            createUser(req.body.username, email, req.body.password)
-                .then(user => {          
-                    console.log("User added!");                    
-                    return done(null, user);   
-                });       
-    });      
-
-}));
-/****************************************************/
-
-/***********Passport Strategy Local-Login************/
-// Logins in the user
-passport.use('local-login', new LocalStrategy({
-    usernameField : 'email',
-    passwordField: 'password',
-    passReqToCallback : true
-},
-(req, email, password, done) => {   
+    /***********Passport Strategy Local-Join************/
+    passport.use('local-join', new LocalStrategy({
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true
+    },
+    (req, email, password, done) => {     
+        // get user with given email. If user already exists return false  
         getUserWithEmail(email)
             .then(user => {
-                console.log("Getting user with email:",email);
-                console.log('Login user object',user);
-                if (!user){
-                    return done(null, false);
-                }                             
-                        
-                if (!User.validPassword(password, user.password)){
-                    console.log("Password is not valid");
+                console.log("Passport local-join",user);  
+                if (user) {
+                    console.log("User already Exists");
                     return done(null, false);    
-                }
-                return done(null, user);          
-                    
-            });
-        
-}));
-/****************************************************/
+                }    
 
-export default passport;
+                // else create and return user                    
+                createUser(req.body.username, email, req.body.password)
+                    .then(user => {          
+                        console.log("User added!");                    
+                        return done(null, user);   
+                    });       
+        });      
+
+    }));
+    /****************************************************/
+
+    /***********Passport Strategy Local-Login************/
+    // Logins in the user
+    passport.use('local-login', new LocalStrategy({
+        usernameField : 'email',
+        passwordField: 'password',
+        passReqToCallback : true
+    },
+    (req, email, password, done) => {   
+            getUserWithEmail(email)
+                .then(user => {
+                    console.log("Getting user with email:",email);
+                    console.log('Login user object',user);
+                    if (!user){
+                        return done(null, false);
+                    }                             
+                            
+                    if (!User.validPassword(password, user.password)){
+                        console.log("Password is not valid");
+                        return done(null, false);    
+                    }
+                    return done(null, user);          
+                        
+                });
+            
+    }));
+    return passport;
+}
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,39 +1,39 @@
-import userDB from './userDB';
 import Sequelize from 'sequelize';
 import bcrypt from 'bcrypt-nodejs';
 
 // Model definition
-const User = userDB.define('user',{
+export let UserModel = function(sequalizeDB){
+  const User = sequalizeDB.define('user',{
     username: {
-        type: Sequelize.STRING
+      type: Sequelize.STRING
     },
     email: {
-        type: Sequelize.STRING,
+      type: Sequelize.STRING,
     },
     password:{ 
-        type: Sequelize.STRING,
+      type: Sequelize.STRING,
     }
-});  
+  });  
 
-// user password encryption
-User.generateHash = (password) => {
+  // user password encryption
+  User.generateHash = (password) => {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+  };
 
-// verify if password is correct
-User.validPassword = (password,userPassword) => {
+  // verify if password is correct
+  User.validPassword = (password,userPassword) => {
     console.log("Validating Password", userPassword);
     return bcrypt.compareSync(password, userPassword);
-};
+  };
 
-// Some mock data. If user table exist 'force' equals true will drop it first
-User.sync({force:true}).then(() => {
+  // Some mock data. If user table exist 'force' equals true will drop it first
+  User.sync({force:true}).then(() => {
     // Table created
     return User.create({
-        username: "Jilian Carlile",
-        email: "jillian.carlile@fakeEmail.com",
-        password: User.generateHash("1234password")
+      username: "Jilian Carlile",
+      email: "jillian.carlile@fakeEmail.com",
+      password: User.generateHash("1234password")
     });
-}); 
-
-export default User; 
+  });
+  return User
+}
