32297e682d7e7d404082a24b1d0429b8f5a74630
James Vaughn <jvaughn575@gmail.com>
2018-02-13 19:05:00 -0600

    Added a belongsTo relations ship between inspiration and user model.

--- server/migrations/20180213070811-update-user.js
+++ server/migrations/20180213070811-update-user.js
@@ -5,12 +5,7 @@ module.exports = {
       await queryInterface.changeColumn('Users', 'profileImg', {        
         type: Sequelize.TEXT('medium'),
         allowNull: true,        
-      }),
-      
-      await queryInterface.changeColumn('Users','id', {
-        type: Sequelize.UUID,
-        default: Sequelize.UUIDV4
-      }),
+      }),      
       
     ]
     
@@ -20,12 +15,7 @@ module.exports = {
       await queryInterface.changeColumn('Users', 'profileImg', {        
         type: Sequelize.BLOB(),
         allowNull: true,
-      }),
-
-      await queryInterface.changeColumn('Users', 'id', {
-        type: Sequelize.INTEGER,
-        autoincrement: true
-      }),
+      }),     
            
     ]  
   }    
--- server/models/inspiration.js
+++ server/models/inspiration.js
@@ -1,26 +1,6 @@
-/*import Sequelize from 'sequelize';
-
-  export const InspirationModel = function(sequelizeDB){
-    const Inspiration = sequelizeDB.define('inspiration', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        default: Sequelize.UUIDV4,
-        type: Sequelize.UUIDV4,        
-      },    
-      image: {
-        type: Sequelize.TEXT,
-      },  
-      description: {
-        type: Sequelize.STRING,
-      }  
-  });
-  return Inspiration;
-  } */
-
   import Sequelize from 'sequelize';
 
-  const InspirationModel = function(sequelizeDB){
+  export let InspirationModel = function(sequelizeDB){
     const Inspiration = sequelizeDB.define('inspiration', {
       id: {
         allowNull: false,
@@ -34,8 +14,12 @@
       description: {
         type: Sequelize.STRING,
       }  
-  });
+    });
+    Inspiration.associate = models => {
+      Inspiration.belongsTo(models.User);
+    }
+
   return Inspiration;
   }
 
-  //export default InspirationModel;
+ 
