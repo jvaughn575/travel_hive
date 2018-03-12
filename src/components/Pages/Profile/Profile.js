673f40781204178dfc622a804e567e6c18c3d25f
James Vaughn <jvaughn575@gmail.com>
2018-03-12 09:33:42 -0500

    Broke out EditProfile component from profile.js to EditProfile.js in profile folder

--- src/components/Pages/Profile/Profile.js
+++ src/components/Pages/Profile/Profile.js
@@ -23,79 +23,9 @@ import {
 
 /***************  Profile folder imports ****************/
 import {Avatar} from "./Avatar";
+import {EditProfile} from "./EditProfile";
 
 const { Meta } = Card;
-
-
-
-
-class EditProfile extends React.Component {
-  state = { visible: false, bioValue: this.props.appState.user.bioText };
-  showModal = () => {
-    this.setState({
-      visible: true
-    });
-  };
-  handleOk = e => {
-    console.log(e);
-    this.setState({
-      visible: false
-    });
-    addBioText(this.state.bioValue).then(bioText => {
-      if (bioText) {
-        message.success("Biography updated!");
-        this.props.appState.dispatch({
-          type: "user/updateBioText",
-          payload: this.state.bioValue
-        });
-      } else {
-        message.error("Biography failed to update!");
-      }
-    });
-    // callback to set state in Profile
-    this.props.updateProfileState({
-      bio: this.state.bioValue
-    });
-  };
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visible: false
-    });
-  };
-  onBioChange = e => {
-    this.setState({
-      bioValue: e.target.value
-    });
-  };
-  render() {
-    const { TextArea } = Input;
-    return (
-      <div>
-        <Button icon="edit" onClick={this.showModal}>
-          Edit Profile
-        </Button>
-        <Modal
-          title="Edit Profile"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <Avatar
-            updateProfileState={this.props.updateProfileState}
-            appState={this.props.appState}
-          />
-          <p>Write a brief description about yourself</p>
-          <TextArea
-            rows={4}
-            onChange={this.onBioChange}
-            value={this.state.bioValue}
-          />
-        </Modal>
-      </div>
-    );
-  }
-}
 
 class PinInput extends Component {
   constructor(props) {
