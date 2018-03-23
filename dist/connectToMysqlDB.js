7dc622a0865c23b6dba716050245a45f4b8153de
James Vaughn <jvaughn575@gmail.com>
2018-03-22 23:46:49 -0500

    more openshift config adjustments

--- dist/connectToMysqlDB.js
+++ dist/connectToMysqlDB.js
@@ -17,6 +17,8 @@ var mysql = require('mysql');
 
 var mysqlURL = process.env.OPENSHIFT_MYSQL_DB_URL || process.env.MYSQL_URL,
     mysqlURLLabel = "";
+
+console.log("My SQL URL: ", process.env.MYSQL_URL);
 /*
 if(mysqlURL == null && process.env.DATABASE_SERVICE_NAME) {
   const mysqlServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
